449f55d74b2ade01064b9e56136ff2578462ce2b
Thomazp01 <lima.thomaz2008@gmail.com>
2025-06-02 07:55:50 -0300

    ATUALIZAÇAO 26/05

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ db.serialize(() => {
     db.run(
         "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
     )
+
+    db.run(
+        "CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, id_users INTEGER, titulo TEXT, conteudo TEXT, data_criacao TEXT)"
+    )
 });
 app.use(
     session({
@@ -63,6 +67,7 @@ app.post("/login", (req, res) => {
         if (row) {
             req.session.loggedin = true;
             req.session.username = username;
+            req.session.id_username = row.id; //Armazena o ID do usuário na sessão
             res.redirect("/dashboard")
         } else {
             res.redirect("/usuario_incorreto")
@@ -87,12 +92,13 @@ app.post("/cadastro", (req, res) => {
 
     db.get(query1, [username], (err, row) => {
         if (err) throw err; 
-
+    
         //1. Verificar se o usuário existe
         console.log(JSON.stringify(row))
         if (row) {
             console.log(`Usuario ${username} já cadastrado`)
             res.redirect("usuario_cadastrado")
+        
         } else {
             db.get(query2, [username, password], (err, row) => {
 
@@ -104,9 +110,40 @@ app.post("/cadastro", (req, res) => {
                 res.redirect("/cadastro_concluido")
             })
         }
-
     })
 });
+app.get("/post_create", (req, res) => {
+    console.log("GET /post_create")
+    //verificar se o usuário está logado 
+    if(req.session.loggedin){
+    //se estiver renderizar a página de criação de post  
+    res.render("pages/post_form", { titulo: "Criar postagem" , req: req}); 
+    } else {
+    //se não estiver redirecionar para a página de usuario invalido
+        res.redirect("/nao_autorizado")
+    }
+})
+app.post("/post_create", (req, res) => {
+    console.log("POST /post_create")
+    if(req.session.loggedin) {
+        console.log("Dados da postagem ", req.body);
+        const { titulo, conteudo } = req.body;
+        const data_criacao = new Date();
+        console.log("Username: ", data_criacao, "username:", req.session.username, "id_username:", req.session.id_username,);
+        const query = "INSERT INTO posts (id_users, titulo, conteudo, data_criacao) VALUES (?, ?, ?, ?)";
+
+        db.get(query, [req.session.id_username, titulo, conteudo, data_criacao], (err, row) => {
+            if (err) throw err; //SE OCORRER O ERRO VÁ PARA O RESTO DO CÓDIGO
+            res.send("Post criado com sucesso");
+
+            //1. Verificar se o usuário existe
+            
+        })
+    } else {
+        res.redirect("/nao_autorizado")
+    }
+})
+
 app.get("/logout", (req, res) => {
     console.log("GET /logout")
     req.session.destroy(() => {
